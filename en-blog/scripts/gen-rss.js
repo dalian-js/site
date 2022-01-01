f913e6283b16e0bef2a34aada410e06637163ad6
geekplux <geekplux@gmail.com>
2022-01-01 18:12:34 +0800

    fix rss

--- en-blog/scripts/gen-rss.js
+++ en-blog/scripts/gen-rss.js
@@ -1,18 +1,47 @@
 const { promises: fs } = require('fs')
 const path = require('path')
-const RSS = require('rss')
 const matter = require('gray-matter')
+const { Feed } = require('feed')
+const { marked } = require('marked')
+const YEAR = new Date().getFullYear()
 
 const SITE = 'GeekPlux'
 const URL = 'https://geekplux.me'
+// const DOMAIN = 'geekplux.me'
+const AVATAR =
+  'https://geekpluxblog.oss-cn-hongkong.aliyuncs.com/avatar.jpg?x-oss-process=style/zip'
+const DESCRIPTION =
+  'Data Visualization & Full-stack programmer @ finance firm, always exploring'
+const FAV = 'https://geekplux.me/favicon.ico'
+// const api = 'https://i.microlink.io/'
+// const cardUrl = (title) =>
+//   `https://cards.microlink.io/?preset=chrisbiscardi&subtitle=${DOMAIN}&name=${SITE}&title=${title}`
+// const coverImage = (title) => `${api}${encodeURIComponent(cardUrl(title))}`
+
+const feed = new Feed({
+  title: SITE,
+  description: DESCRIPTION,
+  id: URL,
+  link: URL,
+  language: 'en', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+  image: AVATAR,
+  favicon: FAV,
+  copyright: `CC BY-NC 4.0 ${YEAR} @ ${SITE}`,
+  // updated: new Date(2013, 6, 14), // optional, default = today
+  // generator: 'awesome', // optional, default = 'Feed for Node.js'
+  feedLinks: {
+    // json: `${URL}/json`,
+    atom: `${URL}/atom.xml`,
+    feed: `${URL}/feed.xml`
+  },
+  author: {
+    name: SITE,
+    // email: '@',
+    link: URL
+  }
+})
 
 async function generate() {
-  const feed = new RSS({
-    title: SITE,
-    site_url: URL,
-    feed_url: `${URL}/feed.xml`
-  })
-
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
 
   await Promise.all(
@@ -23,20 +52,37 @@ async function generate() {
         path.join(__dirname, '..', 'pages', 'posts', name)
       )
       const frontmatter = matter(content)
+      const link = '/posts/' + name.replace(/\.mdx?/, '')
+      let tags = frontmatter.data.tag || frontmatter.data.tags || []
+      tags = (Array.isArray(tags) ? tags : tags.split(',')).map((tag) =>
+        tag.trim()
+      )
 
-      feed.item({
+      feed.addItem({
+        id: link,
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        url: link,
+        link,
         date: frontmatter.data.date,
         description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
+        categories: tags,
+        tags,
+        content: marked.parse(frontmatter.content),
+        // image: coverImage(frontmatter.data.title),
+        author: [
+          {
+            name: SITE,
+            // email: "@",
+            link: URL
+          }
+        ]
       })
     })
   )
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
-  await fs.writeFile('./public/atom.xml', feed.xml({ indent: true }))
+  await fs.writeFile('./public/feed.xml', feed.rss2())
+  await fs.writeFile('./public/atom.xml', feed.atom1())
+  await fs.writeFile('./public/feed.json', feed.json1())
 }
 
 generate()
--- zh-blog/scripts/gen-rss.js
+++ zh-blog/scripts/gen-rss.js
@@ -1,42 +1,92 @@
 const { promises: fs } = require('fs')
 const path = require('path')
-const RSS = require('rss')
 const matter = require('gray-matter')
+const { Feed } = require('feed')
+const { marked } = require('marked')
+const YEAR = new Date().getFullYear()
 
 const SITE = 'GeekPlux'
 const URL = 'https://geekplux.com'
+// const DOMAIN = 'geekplux.com'
+const AVATAR =
+  'https://geekpluxblog.oss-cn-hongkong.aliyuncs.com/avatar.jpg?x-oss-process=style/zip'
+const DESCRIPTION = '数据可视化 & 全栈程序员 @ 金融公司，always exploring'
+const FAV = 'https://geekplux.me/favicon.ico'
+// const api = 'https://i.microlink.io/'
+// const cardUrl = (title) =>
+//   `https://cards.microlink.io/?preset=chrisbiscardi&subtitle=${DOMAIN}&name=${SITE}&title=${title}`
+// const coverImage = (title) => `${api}${encodeURIComponent(cardUrl(title))}`
+
+const feed = new Feed({
+  title: SITE,
+  description: DESCRIPTION,
+  id: URL,
+  link: URL,
+  language: 'zh', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+  image: AVATAR,
+  favicon: FAV,
+  copyright: `CC BY-NC 4.0 ${YEAR} @ ${SITE}`,
+  // updated: new Date(2013, 6, 14), // optional, default = today
+  // generator: 'awesome', // optional, default = 'Feed for Node.js'
+  feedLinks: {
+    // json: `${URL}/json`,
+    atom: `${URL}/atom.xml`,
+    feed: `${URL}/feed.xml`
+  },
+  author: {
+    name: SITE,
+    // email: '@',
+    link: URL
+  }
+})
 
 async function generate() {
-  const feed = new RSS({
-    title: SITE,
-    site_url: URL,
-    feed_url: `${URL}/feed.xml`
-  })
+  async function addFeedItem(name, type) {
+    if (name.startsWith('index.')) return
+
+    const content = await fs.readFile(
+      path.join(__dirname, '..', 'pages', type, name)
+    )
+    const frontmatter = matter(content)
+    const link = `/${type}/` + name.replace(/\.mdx?/, '')
+    let tags = frontmatter.data.tag || frontmatter.data.tags || []
+    tags = (Array.isArray(tags) ? tags : tags.split(',')).map((tag) =>
+      tag.trim()
+    )
+
+    feed.addItem({
+      id: link,
+      title: frontmatter.data.title,
+      url: link,
+      link,
+      date: frontmatter.data.date,
+      description: frontmatter.data.description,
+      categories: tags,
+      tags,
+      content: marked.parse(frontmatter.content),
+      // image: coverImage(frontmatter.data.title),
+      author: [
+        {
+          name: SITE,
+          // email: "@",
+          link: URL
+        }
+      ]
+    })
+  }
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-
+  const newsletters = await fs.readdir(
+    path.join(__dirname, '..', 'pages', 'newsletters')
+  )
+  await Promise.all(posts.map(async (name) => addFeedItem(name, 'posts')))
   await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return
-
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
-
-      feed.item({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: (frontmatter.data.tags ? frontmatter.data.tags : frontmatter.data.tag.split(', ')).map(s => s.trim()),
-        author: frontmatter.data.author
-      })
-    })
+    newsletters.map(async (name) => addFeedItem(name, 'newsletters'))
   )
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
-  await fs.writeFile('./public/atom.xml', feed.xml({ indent: true }))
+  await fs.writeFile('./public/feed.xml', feed.rss2())
+  await fs.writeFile('./public/atom.xml', feed.atom1())
+  await fs.writeFile('./public/feed.json', feed.json1())
 }
 
 generate()
